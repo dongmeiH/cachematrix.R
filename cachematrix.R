1e6477c04f964b533819d15f93971dd9beae4b63
dongmeiH <46196950+dongmeiH@users.noreply.github.com>
2018-12-27 11:54:44 -0500

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,41 @@
+########################## Programming assignment 2
+###### caching  the inverse of a matrix 
+## <<- is a operator which can be used to assign value to an object in an 
+## environment that is different from the current
+# makeCacheMatrix creates a special matrix than can cathe its inverse
+# cacheSolve: computes the inverse of the special matrix returned by makeCatheMatrix
+#1. set the value of the matrix
+#2. get the value of the matrix
+#3. set the value of the inverse
+#4. get the value of the inverse
+
+makeCacheMatrix <- function(x = numeric()){
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function()
+	setinv <- function(solve) inv <<- solve
+	getinv <- function() inv
+	list(set = set, get = get, 
+	    setinv = setinv
+	    getinv = getinv)
+}
+
+
+# computes the inverse of the special matrix returned by makeCatheMatrix
+# however, it first checks to see if the inverse has already been calculated. if so, it 
+# gets the inverse from cache and skip the computation. otherwise, it calculates the inverse
+
+cacheSolve <- function(x, ...) {
+	inv <- x$getinv()
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setinv(inv)
+	inv
+}
